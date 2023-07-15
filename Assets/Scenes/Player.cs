b46861f810d107df68e0cd00194231584795ac6c
José Magalhães <joses.magalhaes@hotmail.com>
2023-07-14 23:41:38 -0300

    Definição de variáveis do player

--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float Speed;
+    public float JumpForce;
+
+    private Rigidbody2D rig;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rig.velocity = new Vector2(Speed, rig.velocity.y);
     }
 }
